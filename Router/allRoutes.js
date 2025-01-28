f69e4b45ba3f87e2ab9a4502802ded881de95b69
Oury Bah <bl.oury94@gmail.com>
2025-01-28 17:17:30 +0900

    connection pages

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,10 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La Galerie", "/pages/galerie.html"),
     new Route("/lacarte", "La Carte", "/pages/lacarte.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/account", "Mon Compte", "/pages/account.html"),
+    new Route("/editpassword", "Modifier Mes Informations", "/pages/editpassword.html"),
 ];
 
     //Le titre s'affiche comme ceci : Route.titre - websitename
